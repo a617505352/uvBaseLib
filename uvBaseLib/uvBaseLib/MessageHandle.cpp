54b7fc857c7d306878e082f7a653bfbf0172350a
chenliang <chenliang@detu.com>
2017-10-22 18:51:21 +0800

    add

--- uvBaseLib/uvBaseLib/MessageHandle.cpp
+++ uvBaseLib/uvBaseLib/MessageHandle.cpp
@@ -1,81 +1,47 @@
 #include <windows.h>
 #include "MessageHandle.h"
-#include "LOG.h"
 
 
-bool CMessageHandle::m_is_closed  = false;
-long CMessageHandle::m_session_id = 10000;
-long CMessageHandle::m_refrence_num = 0;
-
-std::mutex					CMessageHandle::m_ref_mtx;
-std::mutex					CMessageHandle::m_msg_mtx; 
-std::condition_variable		CMessageHandle::m_msg_con;
-std::deque<SigNode>			CMessageHandle::m_messages;
-
-			CMessageHandle::m_message_loop_thread(&CMessageHandle::Looper);
+long		CMessageHandle::m_session_id = 10000;
+long		CMessageHandle::m_refrence_num = 0;
+std::mutex	CMessageHandle::m_refrence_mtx;
+CMessageThread* CMessageHandle::m_message_thread = NULL;
 
 CMessageHandle::CMessageHandle()
 {
-	InterlockedIncrement(&m_refrence_num);
-	m_own_id = InterlockedIncrement(&m_session_id);
 	m_owner = NULL;
+	m_own_id = InterlockedIncrement(&m_session_id);
 
-	m_message_loop_thread.reset(&CMessageHandle::Looper);
+	std::unique_lock<std::mutex> ref_lock(m_refrence_mtx);
+	m_refrence_num++;
+	if (m_message_thread == NULL){
+		m_message_thread = new CMessageThread;
+	}
+	ref_lock.unlock();
 }
 
 CMessageHandle::~CMessageHandle()
 {
-	std::unique_lock<std::mutex> lock(m_msg_mtx);
-	if (!InterlockedDecrement(&m_refrence_num)) {
-		lock.unlock();
-		destroy();
-	} else {
-		lock.unlock();
+	std::unique_lock<std::mutex> ref_lock(m_refrence_mtx);
+	m_refrence_num--;
+	if (m_refrence_num == 0 && m_message_thread != NULL){
+		m_message_thread->Close();
+		m_message_thread = NULL;
 	}
+	ref_lock.unlock();
 }
 
 void CMessageHandle::Notify(CMessageHandle* handle, long session_id, int message_type)
 {
-	SigNode sig_node = { 0 };
-	sig_node.handle = handle;
-	sig_node.message_type = message_type;
-	sig_node.session_id = session_id;
-
-	std::unique_lock<std::mutex> lock(m_msg_mtx);
-	m_messages.push_back(sig_node);
-	m_msg_con.notify_one();
-	lock.unlock();
-
+	if (m_message_thread){
+		m_message_thread->Enqueue(handle, session_id, message_type);
+	}
 	return;
 }
 
 long CMessageHandle::GetID(void)
 {
 	return m_own_id;
-}
-
-void CMessageHandle::Looper()
-{
-	while (true) {
-		if (m_is_closed) {
-			break;
-		}
-
-		//SigNode sig_node = { 0 };
-		//std::unique_lock<std::mutex> lock(m_msg_mtx);
-		//if (m_messages.empty()) {
-		//	m_msg_con.wait(lock);
-		//	if (m_is_closed) {
-		//		break;
-		//	}
-		//}
-		//sig_node = m_messages.front();
-		//m_messages.pop_front();
-		//lock.unlock();
-
-		//sig_node.handle->handle_message(sig_node.session_id, sig_node.message_type);
-	}
-	return;
 }
 
 void CMessageHandle::SetOwner(CMessageHandle* owner)
@@ -85,20 +51,5 @@ void CMessageHandle::SetOwner(CMessageHandle* owner)
 
 void CMessageHandle::handle_message(long session_id, int message_type)
 {
-	return;
-}
-
-void CMessageHandle::destroy(void)
-{
-	m_is_closed = true;
-	std::unique_lock<std::mutex> lock(m_msg_mtx);
-	m_msg_con.notify_one();
-
-	while (m_messages.size() > 0) {
-		m_messages.pop_front();
-	}
-	lock.unlock();
-
-	m_message_loop_thread.join();
 	return;
 }
