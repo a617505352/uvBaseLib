87c429969659aaf4de3a78085edbc7ff88039738
chenliang <chenliang@detu.com>
2018-03-07 18:39:12 +0800

    modify: merge sort ok

--- uvBaseLib/uvBaseLib/Algorithms/SortAlgorithms/MergeSort.cpp
+++ uvBaseLib/uvBaseLib/Algorithms/SortAlgorithms/MergeSort.cpp
@@ -1,16 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "MergeSort.h"
 
 CMergeSort::CMergeSort()
 {
-
+	m_data = NULL;
 }
 
 CMergeSort::~CMergeSort()
 {
-
+	if (m_data){
+		free(m_data);
+	}
 }
 
 void CMergeSort::Sort(int* array, int array_num)
 {
+	if (array_num == 1){
+		return;
+	} 
+	m_data = (int*)malloc(array_num*sizeof(int));
+	memset(m_data, 0, array_num);
 
+	sort(array, 0, array_num - 1);
+}
+
+void CMergeSort::sort(int* array, int low, int high)
+{
+	if (high == low){
+		return;
+	}
+
+	int mid = (low + high) / 2;
+	sort(array, low, mid);
+	sort(array, mid + 1, high);
+	merge_lists(array, low, mid, high);
+}
+
+void CMergeSort::merge_lists(int* array, int low, int mid, int high)
+{
+	int down_length = mid - low + 1;
+	int up_length = high - mid;
+
+	int* down = array + low;
+	int* up = array + mid + 1;
+
+	int i, j;
+	i = j = 0;
+	int idx = 0;
+	while (i<down_length && j<up_length){
+		if (down[i] < up[j]){
+			m_data[idx++] = down[i++];
+		} else {
+			m_data[idx++] = up[j++];
+		}
+	}
+
+	if (j == up_length){
+		for (; i < down_length;){
+			m_data[idx++] = down[i++];
+		}
+	} else {
+		for (; j < up_length;){
+			m_data[idx++] = up[j++];
+		}
+	}
+
+	for (int i = 0; i < high - low + 1; i++){
+		array[low+i] = m_data[i];
+	}
 }
--- uvBaseLib/uvBaseLib/Algorithms/SortAlgorithms/MergeSort.h
+++ uvBaseLib/uvBaseLib/Algorithms/SortAlgorithms/MergeSort.h
@@ -1,6 +1,20 @@
 #ifndef _UV_LIB_MERGER_SORT_H_
 #define _UV_LIB_MERGER_SORT_H_
 
+/**
+* create by chenliang,2018.3.7
+* 归并排序算法:
+*	递归的分成两部分排序，然后合并;
+*
+* MergeSort A[1,...,N]:
+*	1. if N=1 done
+*	2. recursively sort A[1,...,N/2] and A[N/2+1,...,N]
+*	3. merge 2 sorted lits
+*
+* 时间复杂度：O(N*lgN)
+* 空间复杂度：O(N)
+*/
+
 #include "Sort.h"
 
 class CMergeSort : public ISortBase
@@ -12,6 +26,12 @@ public:
 public:
 	virtual void Sort(int* array, int array_num);
 
+private:
+	void sort(int* array, int low, int high);
+	void merge_lists(int* array, int low, int mid, int high);
+
+private:
+	int*	m_data;
 };
 
 #endif
